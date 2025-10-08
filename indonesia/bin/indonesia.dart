5997690c6e6b2ffb2653f43b66c61df195b6332a
billyw03 <yohanesbillywicaksono_2327240155@mhs.mdp.ac.id>
2025-10-08 16:55:37 +0700

    fitur rumus luas dan keliling

--- indonesia/bin/indonesia.dart
+++ indonesia/bin/indonesia.dart
@@ -1,12 +1,16 @@
-import 'package:indonesia/indonesia.dart' as indonesia;
-
 class persegiPanjang {
   int panjang;
   int lebar;
 
   persegiPanjang(this.panjang, this.lebar);
-}
 
-void main(List<String> arguments) {
-  print('Hello world: ${indonesia.calculate()}!');
+  // fungsi luas persegi panjang
+  int luasPersegiPanjang() {
+    return panjang * lebar;
+  }
+
+  // fungsi keliling persegi panjang
+  int kelilingPersegiPanjang() {
+    return 2 * (panjang + lebar);
+  }
 }
