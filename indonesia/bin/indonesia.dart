e26568e1b1aafed183a154e9d6d96e60a2765b01
Nasrudin21 <muhammadnasrudin674@gmail.com>
2025-10-08 14:58:37 +0700

    Update indonesia.dart

--- indonesia/bin/indonesia.dart
+++ indonesia/bin/indonesia.dart
@@ -1,5 +1,3 @@
-import 'package:indonesia/indonesia.dart' as indonesia;
-
 class persegiPanjang {
   int panjang;
   int lebar;
@@ -7,6 +5,5 @@ class persegiPanjang {
   persegiPanjang(this.panjang, this.lebar);
 }
 
-void main(List<String> arguments) {
-  print('Hello world: ${indonesia.calculate()}!');
-}
+//git fetch origin
+//git checkout 1-tolong-buatkan-rumus-perhitungan-luas-dan-keliling-persegi-panjang
