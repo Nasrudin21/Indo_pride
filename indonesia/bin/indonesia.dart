451e4557efc58356cb5bcbc48599421123e79039
Nasrudin21 <muhammadnasrudin674@gmail.com>
2025-10-08 14:46:42 +0700

    dart_persegiPanjang

---
+++ indonesia/bin/indonesia.dart
@@ -0,0 +1,12 @@
+import 'package:indonesia/indonesia.dart' as indonesia;
+
+class persegiPanjang {
+  int panjang;
+  int lebar;
+
+  persegiPanjang(this.panjang, this.lebar);
+}
+
+void main(List<String> arguments) {
+  print('Hello world: ${indonesia.calculate()}!');
+}
---
+++ indonesia/test/indonesia_test.dart
@@ -0,0 +1,8 @@
+import 'package:indonesia/indonesia.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
